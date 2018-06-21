c7f5635d1c1c8746f3549a0ca2e5161433766e8b
Marcus Fields <fieldsmu@gmail.com>
2018-06-21 10:14:17 -0400

    added migrations

---
+++ Assessment1/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Assessment1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Assessment1.Models.AssessmentAppContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Assessment1.Models.AssessmentAppContext";
+        }
+
+        protected override void Seed(Assessment1.Models.AssessmentAppContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
